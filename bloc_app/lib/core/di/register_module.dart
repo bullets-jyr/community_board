b8eee532c92c8b1e75cb1945d4fd1a42847074ce
bullets-jyr <jy.ryu.jiyoung@gmail.com>
2026-02-08 18:09:38 +0900

    presentation: injectable

--- bloc_app/lib/core/di/register_module.dart
+++ bloc_app/lib/core/di/register_module.dart
@@ -1,3 +1,5 @@
+import 'package:data_supabase/auth.dart';
+import 'package:domain/auth.dart';
 import 'package:injectable/injectable.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -5,4 +7,40 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 abstract class RegisterModule {
   @singleton
   SupabaseClient get supabaseClient => Supabase.instance.client;
+
+  // --- Data Layer Registration (LazySingleton) ---
+  // auth
+  @LazySingleton(as: AuthRemoteDataSource)
+  SupabaseAuthRemoteDataSource get authRemoteDataSource;
+
+  @LazySingleton(as: AuthRepository)
+  AuthRepositoryImpl get authRepository;
+
+  // post
+
+
+  // profile
+
+
+  // search
+
+
+  // --- Domain Layer (UseCases) Registration (Injectable - factory) ---
+  // auth
+  @injectable
+  SignupUseCase get signupUseCase;
+
+  @injectable
+  LoginUseCase get loginUseCase;
+
+  @injectable
+  LogoutUseCase get logoutUseCase;
+
+  // post
+
+
+  // profile
+
+
+  // search
 }
