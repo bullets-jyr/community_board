16029515ccb50fc2ea13e60b835f1f2190bf31db
bullets-jyr <jy.ryu.jiyoung@gmail.com>
2026-02-11 18:31:37 +0900

    domain: Post 생성

--- packages/domain/lib/src/post/repositories/post_repository.dart
+++ packages/domain/lib/src/post/repositories/post_repository.dart
@@ -1,11 +1,26 @@
+import 'dart:io';
+
 import 'package:core/errors.dart';
 import 'package:fpdart/fpdart.dart';
 
+import '../dto/image_upload_result.dart';
 import '../entities/post_display.dart';
 
 abstract interface class PostRepository {
   Future<Either<Failure, List<PostDisplay>>> getPosts({
     required int offset,
     required int limit,
+  });
+
+  Future<Either<Failure, PostDisplay>> createPost({
+    String? postId,
+    required String title,
+    required String content,
+    String? imageUrl,
+  });
+
+  Future<Either<Failure, ImageUploadResult>> uploadPostImage({
+    required File image,
+    String? postId,
   });
 }
