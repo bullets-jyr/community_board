a438e1a5d2d8122c1016655266e988cc6d2c828c
bullets-jyr <jy.ryu.jiyoung@gmail.com>
2026-02-11 20:55:19 +0900

    domain: Post Detail

--- packages/domain/lib/src/post/repositories/post_repository.dart
+++ packages/domain/lib/src/post/repositories/post_repository.dart
@@ -4,6 +4,7 @@ import 'package:core/errors.dart';
 import 'package:fpdart/fpdart.dart';
 
 import '../dto/image_upload_result.dart';
+import '../entities/comment_display.dart';
 import '../entities/post_display.dart';
 
 abstract interface class PostRepository {
@@ -22,5 +23,13 @@ abstract interface class PostRepository {
   Future<Either<Failure, ImageUploadResult>> uploadPostImage({
     required File image,
     String? postId,
+  });
+
+  Future<Either<Failure, PostDisplay>> getPostDetail({required String postId});
+
+  Future<Either<Failure, List<CommentDisplay>>> getComments({
+    required String postId,
+    required int offset,
+    required int limit,
   });
 }
