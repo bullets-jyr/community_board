e55738982e7c8b7674c3b609a843e508abb843f0
bullets-jyr <jy.ryu.jiyoung@gmail.com>
2026-02-11 11:53:42 +0900

    presentation: PostCard widget

--- bloc_app/lib/features/post/presentation/pages/post_page.dart
+++ bloc_app/lib/features/post/presentation/pages/post_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../core/di/di.dart';
 import '../blocs/post_list/post_list_bloc.dart';
+import '../widgets/post_card.dart';
 
 class PostPage extends StatelessWidget {
   const PostPage({super.key});
